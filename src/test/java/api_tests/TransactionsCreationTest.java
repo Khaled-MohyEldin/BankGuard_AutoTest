b9deb980e0a1cc77d73bd58bf51637ec00527e0b
Khaled-MohyEldin <khaledtitan2525@gmail.com>
2025-09-29 16:52:07 +0300

    Seperated some Tests

--- src/test/java/api_tests/TransactionsCreationTest.java
+++ src/test/java/api_tests/TransactionsCreationTest.java
@@ -181,48 +181,4 @@ public class TransactionsCreationTest {
     }
 
 
-        @Test
-    public void readAllTranactions() {
-        Response res = TransEP.getAllTrans();
-        System.out.println(res.asPrettyString());
-        Assert.assertEquals(res.getStatusCode(), 200);
-
-        // checking against schema
-        res.then().assertThat()
-        .body(JsonSchemaValidator.matchesJsonSchemaInClasspath("SchemaTransactionAll.json"));
-    }
-
-    @Test
-    public void readUsersTransactionByAccID() {
-        Response res = TransEP.getTransByAccID(6);
-        System.out.println(res.asPrettyString());
-        Assert.assertEquals(res.getStatusCode(), 200);
-
-        // checking against schema
-        res.then().assertThat()
-        .body(JsonSchemaValidator.matchesJsonSchemaInClasspath("SchemaTransactionAll.json"));
-    }
-
-    @Test
-    public void readTransactionByID() {
-        Response res = TransEP.getTransByID(4);
-        System.out.println(res.asPrettyString());
-        Assert.assertEquals(res.getStatusCode(), 200);
-
-        // checking against schema
-        res.then().assertThat()
-        .body(JsonSchemaValidator.matchesJsonSchemaInClasspath("SchemaTransaction.json"));
-    }
-
-    @Test
-    public void readTransactionByReference() {
-        Response res = TransEP.getTransByRef("TXN-58B68304");
-        System.out.println(res.asPrettyString());
-        Assert.assertEquals(res.getStatusCode(), 200);
-
-        // checking against schema
-        res.then().assertThat()
-        .body(JsonSchemaValidator.matchesJsonSchemaInClasspath("SchemaTransaction.json"));
-    }
-
 }
